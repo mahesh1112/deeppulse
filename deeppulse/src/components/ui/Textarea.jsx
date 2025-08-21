6f0bcf5a731312099b4cd0a034d4b0196213c010
Mahesh Shriram <98745824+mahesh1112@users.noreply.github.com>
2025-08-21 20:29:59 +0530

    Delete deeppulse directory

--- deeppulse/src/components/ui/Textarea.jsx
+++
@@ -1,9 +0,0 @@
-export function Textarea({ className = "", ...props }) {
-    return (
-      <textarea
-        className={`w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
-        {...props}
-      />
-    )
-  }
-  
